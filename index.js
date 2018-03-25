4a79bc914361e669dee70f89561dc7b8530585dd
daniel contreras <daniel@sciensomnia.com>
2018-03-25 11:32:51 -0500

    Added Base Code

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+const ccxt = require('ccxt');
+const sqlite3 = require('sqlite3');
+var db;
+
+
+connectToDB();
+
+getTable();
+
+setInterval(()=>{
+    getTable();
+}, 5000)
+
+function connectToDB() {
+    db = new sqlite3.Database('./ask-bid.db', (err) => {
+        if (err) return console.error(err.message)
+        console.log('Successfully connected to in-memory SQL database')
+    })
+
+    db.serialize(function () {
+        db.run("CREATE TABLE if not exists kraken (timestamp TEXT, ask TEXT, bid TEXT)");
+    });
+}
+
+function closeDB(){
+    db.close();
+}
+
+function getTable() {
+    let kraken    = new ccxt.kraken ()
+    kraken.fetchOrderBook ('ETH/USD').then(res=>{
+        db.run(`INSERT into kraken (timestamp, ask, bid) VALUES ("${res.timestamp}", "${escape(JSON.stringify(res.asks))}", "${escape(JSON.stringify(res.bids))}")`)
+        console.log('Added entry to kraken table')
+    })
+}
